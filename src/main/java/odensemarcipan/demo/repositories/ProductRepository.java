83132fa774d07b3fd83f304f4e06e241ef4321c7
Andreas-Hjort <73465555+Andreas-Hjort@users.noreply.github.com>
2021-12-09 11:21:55 +0100

    linked index med thymeleaf

--- src/main/java/odensemarcipan/demo/repositories/ProductRepository.java
+++ src/main/java/odensemarcipan/demo/repositories/ProductRepository.java
@@ -2,6 +2,7 @@ package odensemarcipan.demo.repositories;
 
 import odensemarcipan.demo.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 }
