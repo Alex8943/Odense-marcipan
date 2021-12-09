ea1c35e56f6913306cd99c43ceb73b8cd30b3ae7
Lasse <knoefler12>
2021-12-09 04:48:45 +0100

    trying to make project work with thymeleaf

--- src/main/java/odensemarcipan/demo/controllers/Products.java
+++ src/main/java/odensemarcipan/demo/controllers/Products.java
@@ -5,10 +5,8 @@ import odensemarcipan.demo.repositories.ProductRepository;
 import odensemarcipan.demo.repositories.ShoppingCartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class Products {
@@ -40,13 +38,15 @@ public class Products {
    }
 
     @GetMapping("/products/{id}")
-    public String singleProduct(@PathVariable int id){
+    public String singleProduct(@PathVariable int id,Model model){
+        model.addAttribute("products",productRepository.findAll());
         return "/single-products/singleProduct-"+id;
     }
 
 
     @PostMapping("/products")
-    public void addToShoppingCart(@ModelAttribute ShoppingCart shoppingCart){
+    public void addToShoppingCart(@RequestParam String id, @ModelAttribute ShoppingCart shoppingCart){
+        System.out.println("yeet");
         shoppingCartRepository.save(shoppingCart);
     }
 
